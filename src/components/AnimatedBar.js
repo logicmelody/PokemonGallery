99d96a4e3c6684e2d93a388dcda6d85b3b165885
Danny Lin <danny.l@ljit.io>
2019-05-07 18:04:25 +0800

    Animation for AnimatedBar

--- src/components/AnimatedBar.js
+++ src/components/AnimatedBar.js
@@ -1,19 +1,56 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import {
+	View,
+	Animated, // add this
+	Easing // add this
+} from "react-native";
 
-type Props = {};
-export default class AnimatedBar extends Component<Props> {
-  render() {
-    let widthStyle = { width: this.props.value };
-    return <View style={[styles.bar, widthStyle]} />;
-  }
+class AnimatedBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.width = new Animated.Value(0);
+	}
+
+	animateBar = () => {
+		const { value, index } = this.props;
+
+		this.width.setValue(0); // initialize the animated value
+
+		Animated.timing(this.width, {
+			toValue: value,
+			delay: index * 150 // how long to wait before actually starting the animation
+		}).start();
+	};
+
+	render() {
+		let barWidth = {
+			width: this.width
+		};
+
+		return (
+			<Animated.View
+				style={[styles.bar, barWidth]}
+			/>
+		);
+	}
+
+	componentDidMount() {
+		this.animateBar();
+	}
+
+	componentDidUpdate() {
+		this.animateBar();
+	}
 }
 
 const styles = {
-  bar: {
-    height: 15,
-    borderWidth: 1,
-    borderColor: "#c72f06",
-    backgroundColor: "#e75832"
-  }
+	bar: {
+		height: 15,
+		borderWidth: 1,
+		borderColor: "#c72f06",
+		backgroundColor: "#e75832"
+	}
 };
+
+export default AnimatedBar;
