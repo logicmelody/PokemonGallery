6921c13f5363e8f1d5360ee4081a14f76519cb7e
Danny Lin <danny.l@ljit.io>
2019-05-07 18:54:03 +0800

    Sequence animation

--- src/components/BigCard.js
+++ src/components/BigCard.js
@@ -1,24 +1,20 @@
 import React, { Component } from "react";
-import { View, Text, Image } from "react-native";
+import {
+	View,
+	Text,
+	Image,
+	Animated, // add this
+	Easing // add this
+} from "react-native";
 import DataRow from "./DataRow";
 
 class BigCard extends Component {
-	render() {
-		const { image, title, data } = this.props;
+	constructor(props) {
+		super(props);
 
-		return (
-			<View style={styles.container}>
-				<View style={styles.mainContainer}>
-					<Image source={image} style={styles.image} resizeMode={"contain"} />
-					<View style={styles.titleContainer}>
-						<Text style={styles.title}>{title}</Text>
-					</View>
-				</View>
-				{data && (
-					<View style={styles.dataContainer}>{this.renderDataRows(data)}</View>
-				)}
-			</View>
-		);
+		this.imageOpacityValue = new Animated.Value(0);
+		this.titleTranslateYValue = new Animated.Value(0);
+		this.titleScaleValue = new Animated.Value(0);
 	}
 
 	renderDataRows = data => {
@@ -33,6 +29,83 @@ class BigCard extends Component {
 			);
 		});
 	};
+
+	render() {
+		const { image, title, data } = this.props;
+
+		const imageOpacity = this.imageOpacityValue.interpolate({
+			inputRange: [0, 0.25, 0.5, 0.75, 1],
+			outputRange: [0, 0.25, 0.5, 0.75, 1]
+		});
+
+		// construct the image style
+		const imageOpacityStyle = {
+			opacity: imageOpacity
+		};
+
+		// interpolate the vertical position of the title
+		const titleMoveY = this.titleTranslateYValue.interpolate({
+			inputRange: [0, 1],
+			outputRange: [0, 280]
+		});
+
+		// interpolate the scale of the title
+		const titleScale = this.titleScaleValue.interpolate({
+			inputRange: [0, 0.5, 1],
+			outputRange: [0.25, 0.5, 1]
+		});
+
+		// construct the styles for the title
+		const titleTransformStyle = {
+			transform: [{ translateY: titleMoveY }, { scale: titleScale }]
+		};
+
+		return (
+			<View style={styles.container}>
+				<View style={styles.mainContainer}>
+					<Animated.Image
+						source={image}
+						style={[styles.image, imageOpacityStyle]}
+						resizeMode={"contain"}
+					/>
+
+					<Animated.View style={[styles.titleContainer, titleTransformStyle]}>
+						<Text style={styles.title}>{title}</Text>
+					</Animated.View>
+				</View>
+
+				{data && (
+					<View style={styles.dataContainer}>{this.renderDataRows(data)}</View>
+				)}
+			</View>
+		);
+	}
+
+	componentDidUpdate() {
+		// reset the animated values
+		this.imageOpacityValue.setValue(0);
+		this.titleTranslateYValue.setValue(0);
+		this.titleScaleValue.setValue(0);
+
+		// start the sequence
+		Animated.sequence([
+			Animated.timing(this.imageOpacityValue, {
+				toValue: 1,
+				duration: 1000,
+				easing: Easing.linear
+			}),
+			Animated.timing(this.titleTranslateYValue, {
+				toValue: 1,
+				duration: 300,
+				easing: Easing.linear
+			}),
+			Animated.timing(this.titleScaleValue, {
+				toValue: 1,
+				duration: 300,
+				easing: Easing.linear
+			})
+		]).start();
+	}
 }
 
 const styles = {
@@ -56,7 +129,11 @@ const styles = {
 		flex: 2,
 		flexDirection: "column",
 		padding: 20
-	}
+	},
+	titleContainer: {
+		position: "absolute",
+		top: -100
+	},
 };
 
 export default BigCard;
