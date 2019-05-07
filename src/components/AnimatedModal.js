ea30c6c1c8681a9699caa1e95014baf9f67251f6
Danny Lin <danny.l@ljit.io>
2019-05-07 17:34:57 +0800

    Spring animation for AnimatedModal

--- src/components/AnimatedModal.js
+++ src/components/AnimatedModal.js
@@ -1,25 +1,75 @@
 import React, { Component } from "react";
-import { View, Text, Dimensions, TouchableOpacity } from "react-native";
+import {
+	View,
+	Text,
+	Dimensions,
+	TouchableOpacity,
+	Animated, // add this
+	Easing // add this
+} from "react-native";
 
 import Header from "./Header";
 
 const { height, width } = Dimensions.get("window");
 
 class AnimatedModal extends Component {
+	constructor(props) {
+		super(props);
+
+		// declare animated value for controlling the vertical position of the modal
+		this.yTranslate = new Animated.Value(0);
+	}
+
 	render() {
-		const { title, image, children, onClose } = this.props;
+		const {
+			title,
+			image,
+			children,
+			onClose,
+		} = this.props;
+
 		let bottomStyle = this.props.visible ? { bottom: 0 } : { bottom: -height };
 
+		let negativeHeight = -height + 20;
+		let modalMoveY = this.yTranslate.interpolate({
+			inputRange: [0, 1],
+			outputRange: [0, negativeHeight]
+		});
+
+		// translateY is the transform for moving objects vertically
+		let translateStyle = { transform: [{ translateY: modalMoveY }] };
+
 		return (
-			<View style={[styles.container, bottomStyle]}>
+			<Animated.View style={[styles.container, translateStyle]}>
 				<Header title={title}>
 					<TouchableOpacity onPress={onClose}>
 						<Text style={styles.closeText}>Close</Text>
 					</TouchableOpacity>
 				</Header>
+
 				<View style={styles.modalContent}>{children}</View>
-			</View>
+			</Animated.View>
 		);
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (this.props.visible) {
+			// animate the showing of the modal
+			this.yTranslate.setValue(0); // reset the animated value
+
+			Animated.spring(this.yTranslate, {
+				toValue: 1,
+				friction: 6
+			}).start();
+
+		} else {
+			// animate the hiding of the modal
+			Animated.timing(this.yTranslate, {
+				toValue: 0,
+				duration: 200,
+				easing: Easing.linear
+			}).start();
+		}
 	}
 }
 
